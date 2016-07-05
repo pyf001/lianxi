e9910aa13127daf76c4ff545eecb9498c463138b
pyf001 <857735982@qq.com>
2016-07-05 11:46:31 +0800

    1.0

--- src/lianxi/test.java
+++ src/lianxi/test.java
@@ -3,9 +3,10 @@ package lianxi;
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("good");
-		
+		// TODO Auto-generated method stub		
+		for(int i = 0;i<10;i++){
+			System.out.println("good");
+		}
 	}
 
 }
